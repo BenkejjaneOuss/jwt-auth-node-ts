64dfb259c3c4d6795d75a9d9c773c394e328fda3
Oussama Benkejjane <oussama.benkejjane@gmail.com>
2023-04-29 15:59:09 +0100

    add env and default config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+require('dotenv').config();
 import express, {Application, Request, Response, NextFunction} from 'express';
+import config from 'config';
 
 const app: Application = express();
+const port = config.get<number>('port');
 
 app.get('/', (req: Request, res: Response) => {
     res.send('The current route is /');
 });
 
-app.listen(5000, () => {console.log('server running')});
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+});
